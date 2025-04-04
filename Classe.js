6a545d46f001f3252da16792da8f9ab5c5c490b5
Erick <erick_schuck@estudante.sesisenai.org.br>
2025-04-04 10:42:44 -0300

    quase pronto

--- Classe.js
+++ Classe.js
@@ -1,28 +1,80 @@
-class Obj{
-    constructor(x,y,w,h,a){
+class Obj {
+    constructor(x, y, w, h, a, speed) {
         this.x = x
         this.y = y
         this.w = w
         this.h = h
         this.a = a
+        this.speed = speed
     }
 
-    des_img(){
+    des_img() {
         let img = new Image()
         img.src = this.a
-        des.drawImage(img,this.x, this.y, this.w, this.h)
+        des.drawImage(img, this.x, this.y, this.w, this.h)
     }
 }
 
-class Jog extends Obj{
+class Jog extends Obj {
     dir = 0
+    pts = 0
+    vida = 5
 
-    mov(){
+    mov() {
         this.y += this.dir
-        if(this.y <= 0){
+        if (this.y <= 0) {
             this.y = 0
-        }else if(this.y >=  600){
-            this.y = 600
+        } else if (this.y >= 550) {
+            this.y = 550
         }
+    }
+    colid(objeto) {
+        if ((this.x < objeto.x + objeto.w) &&
+            (this.x + this.w > objeto.x) &&
+            (this.y < objeto.y + objeto.h) &&
+            (this.y + this.h > objeto.y)) {
+            return true
+        } else {
+            false
+        }
+    }
+    point(objeto) {
+        if (objeto.x <= -100) {
+            return true
+        } else {
+            false
+        }
+    }
+}
+
+class Inimigo extends Obj {
+    mov() {
+        this.x -= this.speed
+        if (this.x <= -110) {
+            this.x = 910
+            this.recomeca()
+        }
+    }
+    recomeca() {
+        this.x = 910
+        this.y = Math.floor(Math.random() * ((500 - 100 + 1) + 100)) // quando o carro sair da tela
+    }
+}
+
+class Bg extends Obj {
+    mov() {
+        this.x -= this.speed
+        if (this.x <= -800) {
+            this.x = 600
+        }
+    }
+}
+
+class Text {
+    des_text(text, x, y, cor, font) {
+        des.fillStyle = cor
+        des.lineWidth = '5'
+        des.font = font
+        des.fillText(text, x, y)
     }
 }
--- prog_main.js
+++ prog_main.js
@@ -2,12 +2,27 @@ let des = document.getElementById('des').getContext('2d')
 
 let foguete = new Jog(10,275,100,50,'./img/foguete.png')
 
-let bge = new Obj(0,0,800,600,'./img/espaco.jpg')
+let bge = new Bg(0,0,800,600,'./img/espaco.jpg', 5)
+let bge2 = new Bg(800,0,800,600,'./img/espaco.jpg', 5)
+let bge3 = new Bg(1600,0,800,600,'./img/espaco.jpg', 5)
+
+let enemy01 = new Inimigo(900,150,60,60,'./img/meteoro1.png', 5)
+let enemy02 = new Inimigo(1200,350,100,100,'./img/planeta1.png', 5)
+let enemy03 = new Inimigo(1500,350,100,100,'./img/jupiter1.png', 5)
+let enemy04 = new Inimigo(1700,350,150,150,'./img/planetavm1.png', 5)
+
+let t1 = new Text()
+let t2 = new Text()
+let t3 = new Text()
+let t4 = new Text()
+let t5 = new Text()
+
+let jogar = true
 
 document.addEventListener('keydown', (e)=>{
     if(e.key === "ArrowUp"){
         foguete.dir = -10
-    }else if(e.key === "ArrowUp"){
+    }else if(e.key === "ArrowDown"){
         foguete.dir = +10
     }
 })
@@ -20,16 +35,92 @@ document.addEventListener('keyup',(e)=>{
     }
 })
 
+function game_over(){
+    if(foguete.vida <=0){
+        jogar = false
+    
+        //motor.pause()
+        // música com o jogo parado
+    }
+}
+
+function pontos(){
+    if(foguete.point(enemy01)){
+        foguete.pts +=5
+    }else if(foguete.point(enemy02)){
+        foguete.pts += 5
+    }else if(foguete.point(enemy03)){
+        foguete.pts += 5
+    }else if(foguete.point(enemy04)){
+        foguete.pts += 5
+    }
+
+    }
+
+function colisao(){
+    if(foguete.colid(enemy01)){
+        foguete.vida -= 1
+        enemy01.recomeca()
+    }else if(foguete.colid(enemy02)){
+        foguete.vida -= 1
+        enemy02.recomeca()
+    }else if(foguete.colid(enemy03)){
+        foguete.vida -= 1
+        enemy03.recomeca()
+    }else if(foguete.colid(enemy04)){
+        foguete.vida -= 1
+        enemy04.recomeca()
+    } 
+}
+
 function atualizar(){
-foguete.mov()
+    if(foguete.pts >= 100) {
+        enemy01.speed = 10
+        enemy02.speed = 10
+        enemy03.speed = 10
+        enemy04.speed = 10
+        bge.speed = 10
+        bge2.speed = 10
+        bge3.speed = 10
+    }
+
+    bge.mov()
+    bge2.mov()
+    bge3.mov()
+    if(jogar){
+        foguete.mov()
+        enemy01.mov()
+        enemy02.mov()
+        enemy03.mov()
+        enemy04.mov()
+        colisao()
+        pontos()
+        game_over()
+
+    }
 
 }
 
 function desenha(){
     bge.des_img()
-    foguete.des_img()
+    bge2.des_img()
+    bge3.des_img()
+    t1.des_text('Pontos:',20,30,'yellow','26px Times')
+    t2.des_text(foguete.pts,104,30,'yellow','26px Times')
+    t3.des_text('Vida:',660,30,'yellow','26px Times')
+    t4.des_text(foguete.vida,720,30,'yellow','26px Times')
+    if(jogar){
+        foguete.des_img()
+        enemy01.des_img()
+        enemy02.des_img()
+        enemy03.des_img()
+        enemy04.des_img()
+        
+    }else {
 
-}
+        t5.des_text('Game Over',260,340,'yellow','46px Times')
+    }  
+    }
 
 function main(){
     des.clearRect(0,0,800,600)
